6126003b65f98c4a0b63329afae502852cca3aea
Morten Matthiesen <mmb@kvalitetsit.dk>
2023-01-19 14:00:58 +0100

    Fix to lower

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+tag_lower=$(echo $1 | tr '[:upper:]' '[:lower:]')
 # Build docker images.
-docker build --tag ${1@L} --label org.opencontainers.image.source=$2 ./node-red
+docker build --tag $tag_lower --label org.opencontainers.image.source=$2 ./node-red
